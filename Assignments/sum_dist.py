f67de1551cd8cc4f01471773e871a683a512e8cb
ChanHyeok-Choi <chan4184@unist.ac.kr>
2023-05-06 17:19:29 +0900

    Assignments initial commit

---
+++ Assignments/sum_dist.py
@@ -0,0 +1,17 @@
+"""
+6. (2 points) Given an array A consisting of n positive integers, let's assume that we want to find a
+positive integer k such that the total sum of the distances between all elements of A and k
+becomes minimized, i.e., sum = min ∑ d_i from i=1 to n , where d_i = |A[i] - k| is the distance between A[i] and
+k. Write real code (not pseudocode) that returns sum (not k), given an array A of length n. Write
+(type) your code with a keyboard and upload it as a separate file.
+● Your code should be written either in C or Python. It should be compiled and run (Zero points if
+the uploaded code does not run).
+● Do not use built-in C or Python libraries that directly solve the problem.
+● Upload your code as a separate file
+● Only the code uploaded to BlackBoard will be graded (not the code on your computer).
+"""
+
+def sum_dist(A, n):
+    sum = 0
+    # Your code
+    return sum
